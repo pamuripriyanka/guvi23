04b9eff3121810501b337b7ba1a59fb623d1853a
darlasrujana <41771819+darlasrujana@users.noreply.github.com>
2018-07-27 00:50:40 -0700

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a = 10, b = 20, c = 7;
+
+	if( a >= b && a >= c)
+		System.out.println(a+" is the largest Number");
+
+	 else if (b >= a && b >= c)
+		 System.out.println(b+" is the largest Number");
+
+	 else
+		 System.out.println(c+" is the largest Number");
+			
+				
+	}
+}
