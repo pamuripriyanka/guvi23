4a0761e68d7e2935262e5fe80b7db674f7ef2427
pamuripriyanka <41770146+pamuripriyanka@users.noreply.github.com>
2018-07-30 23:54:05 -0700

    Update pos.java

--- pos.java
+++ pos.java
@@ -5,22 +5,23 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class pos
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int num=124;
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
 		if(num>0)
 		{
-			System.out.println(num+ "is a positive number");
+			System.out.println("Positive");
 		}
 			else if(num<0)
 			{
-				System.out.println(num+"is a negative number");
+				System.out.println("Negative");
 			}
 				else
 				{
-					System.out.println(num+" is either positive or negative");
+					System.out.println("Zero");
 				}
 		
 	}
